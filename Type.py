e34a22f31d75b2929f39baf594c6b295a8c82637
Saee Pujari <pujari.saee@gmail.com>
2024-06-17 00:54:18 +0100

    New Type enum - diff types of entry

---
+++ Type.py
@@ -0,0 +1,10 @@
+from enum import Enum
+
+class Type(Enum):
+    STRING = "string"
+    NUMBER = "number"
+    SYMBOL = "symbol"
+    INVALID = "invalid"
+
+    def __str__(self):
+        return self.value
