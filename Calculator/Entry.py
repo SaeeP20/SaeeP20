c2b37d1254986d09c1e614fde7599ddc1feb6642
Saee Pujari <pujari.saee@gmail.com>
2024-06-20 16:55:56 +0100

    Fixed imports, added comments

--- Calculator/Entry.py
+++ Calculator/Entry.py
@@ -1,9 +1,8 @@
-import Type
-import Symbol
-import BadTypeException
+from Enums import Symbol, Type
+from CustomExceptions import BadTypeException 
 
 class Entry:
-    
+    # Constructor for all possible instances of Entry
     def __init__(self, num = None, sign = None, input_str = None):
         self.num = None
         self.sign = None
@@ -12,40 +11,40 @@ class Entry:
         self.value_string = None
         self.entry_type = Type.INVALID
 
+    # Sets values for every possible argument type (Float, Symbol, String)
         if isinstance(num, float):
             self.num = num
             self.entry_type = Type.NUMBER
             self.value_string = str(num)
-
         elif isinstance(sign, Symbol):
             self.sign = sign
-            if sign != Symbol.INVALID:
-                self.entry_type = Type.SYMBOL
+            if sign == Symbol.INVALID:
+                raise 
+            self.entry_type = Type.SYMBOL    
             self.value_string = str(sign)
-        
         elif isinstance(input_str, str):
             self.input = input
             self.entry_type = Type.STRING
             self.value_string = input_str
-        
+
+    # Getters for all instances of Entry    
     def get_type(self):
         return self.entry_type
-    
     def get_num(self):
         if self.entry_type != Type.NUMBER:
             raise BadTypeException("Bad Type: The 'Float' value doesn't exist for this item")
         return self.num
-    
     def get_sign(self):
         if self.entry_type != Type.SYMBOL:
             raise BadTypeException("Bad Type: The 'Sign' value doesn't exist for this item")
         return self.sign
-    
     def get_string(self):
         if self.entry_type != Type.STRING:
             raise BadTypeException("Bad Type: The 'String' value doesn't exist for this item")
         return self.input
     
+
+    # Function to determine if two Entries are equal
     def __eq__(self, other):
         if not isinstance(other, Entry):
             return False
@@ -58,6 +57,7 @@ class Entry:
                 return self.string == other.string
         return False
     
+    # Assigns a unique hashcode to each Entry
     def __hash__(self):
         prime = 31
         result = 0
