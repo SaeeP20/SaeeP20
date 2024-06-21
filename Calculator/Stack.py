f83e738988eb89111f1476ff3f58e12a299de6cb
Saee Pujari <pujari.saee@gmail.com>
2024-06-21 03:05:18 +0100

    comments for functions

--- Calculator/Stack.py
+++ Calculator/Stack.py
@@ -2,21 +2,26 @@ from CustomExceptions import EmptyStackException
 import Entry
 
 class Stack(Entry):
+    # Constructor for new stack
     def __init__(self):
         self.items = []
 
+    # Returns the size of the stack
     def sizeof(self):
         return len(self.items)
 
+    # Adds a new item to top of stack
     def push(self, item):
         self.items.append(item)
 
+    # Removes and returns the item at the top of the stack
     def pop(self):
         if self.is_empty():
             raise EmptyStackException("There is nothing to pop")
         return self.items.remove[self.sizeof - 1]
-
+    
+    # Returns the item at the top of the stack
     def top(self):
         if self.is_empty():
             raise EmptyStackException("There is nothing at the top of empty stack")
-        return self.items[-1]
+        return self.items[self.sizeof - 1]
